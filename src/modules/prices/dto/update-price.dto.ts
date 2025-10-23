7cea3d5830f002a4db834e0cde3f53934084a8fb
Dilanjana <dilanjana@webmotech.com>
2025-10-23 12:58:05 +0530

    dev:updated version

--- src/modules/prices/dto/update-price.dto.ts
+++
@@ -1,5 +0,0 @@
-// src/modules/prices/dto/update-price.dto.ts
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePriceDto } from './create-price.dto';
-
-export class UpdatePriceDto extends PartialType(CreatePriceDto) {}
